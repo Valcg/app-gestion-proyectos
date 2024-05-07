e0de8f7d6f46067c8baf8a7d5758f052bb715dd0
valeria <you@example.com>
2024-05-07 10:47:48 +0200

    (core): haciendo la interfaz que implementa los genericos DaoImplMy8PJpa y la clase que adapta el generico a la clase que usamos Departamento

---
+++ 14_Proyecto_para_evaluacion_profe/src/modelo/dao/DepartamentoDao.java
@@ -0,0 +1,9 @@
+package modelo.dao;
+
+import modelo.entidades.Departamento;
+
+public interface DepartamentoDao  extends InterfacegenericoCrud<String,Departamento> {
+
+	
+	
+}
---
+++ 14_Proyecto_para_evaluacion_profe/src/modelo/dao/DepartamentoDaoImplMy8Jpa.java
@@ -0,0 +1,59 @@
+package modelo.dao;
+
+import java.util.List;
+import modelo.entidades.Departamento;
+
+public class DepartamentoDaoImplMy8Jpa  extends abstractDaoImplMy8Jpa 
+
+implements DepartamentoDao {
+
+	@Override
+	public boolean alta(Departamento obj) {
+		try {
+			tx.begin();
+			em.persist(obj);// insert
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();// capturamos el error
+			return false;
+		}
+		
+	}
+
+	@Override
+	public Departamento eliminar(String clave) {
+		try {
+			Departamento dep  = buscarUno(clave);
+			if(dep  != null) {
+				// que arranque la trnasaccio para poder hacer el commit 
+				tx.begin();
+					em.remove(dep);
+				tx.commit();
+				return dep;
+			}else {
+				return null;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+
+	@Override
+	public Departamento buscarUno(String clave) {
+		return em.find(Departamento.class, clave);
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Departamento> buscarTodos() {
+		jpql = "select d from Departamento d";
+		query = em.createQuery(jpql);
+		return query.getResultList();
+	}
+
+}
